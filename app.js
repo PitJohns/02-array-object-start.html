27f19fd26ffbb3a489e539d7c6b71be665c864d2
Veniamin Evstratov <dzimmutenno@gmail.com>
2025-02-03 19:27:22 +0500

    add app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+/* Theory 
+
+const array =  [1, 2, 3, 5, 20, 42]; 
+//const arrayStrings = ['a', 'b', 'c', null, 17];
+//const array = new Array(1, 2, 3, 5, 20, 42);
+
+// console.log(array.length );
+console.log(array [0]);
+console.log(array [array.length - 1]);
+//console.log(array [10]);
+array[0] = 'Privet! ';
+console.log(array);
+array[array.length] = 'becon';*/
+
+ 1:53:50
