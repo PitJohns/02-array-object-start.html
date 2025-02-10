c88008fed699b65cd9bdc487eabcc5967987563f
Veniamin Evstratov <dzimmutenno@gmail.com>
2025-02-10 19:55:04 +0500

    adde object

--- app.js
+++ app.js
@@ -18,24 +18,59 @@ array[array.length] = 'becon';
 
  //console.log(inputElement.value );
 
+const notes = ['рассказать блок про массивы', 'рассказать теорию объектов', 42]
+
+function render() {
+    /*for (let i = 0; i < notes.length; i++) {
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]))
+    }*/
+   for (let note of notes) {
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note))
+   }
+};
+
+render();
+
  createBtn.onclick = function () {
     if (inputElement.value.length === 0) {
-        return 
+        return
     }
      listElement.insertAdjacentHTML(
         'beforeend',
-        `
-            <li class="list-group-item d-flex justify-content-between 
-                align-items-center"
-            >
-            <span>${inputElement.value}</span>
-            <span>
-                <span class="btn btn-small btn-success">&check;</span>
-                <span class="btn btn-small btn-danger">&times;</span>
-            </span>
-            </li>`
+        getNoteTemplate(inputElement.value)
     );
     inputElement.value = '';  
  };
 
- 2:02 
+ function getNoteTemplate(title) {
+    return `
+     <li class="list-group-item d-flex justify-content-between 
+        align-items-center"
+    >
+        <span>${title}</span>
+        <span>
+            <span class="btn btn-small btn-success">&check;</span>
+            <span class="btn btn-small btn-danger">&times;</span>
+        </span>
+        </li>`
+ };
+
+ /**
+  * Object Theory 
+  */
+ const person = {
+    firstName: 'Vladilen',
+    lastName: 'Minin',
+    year: 1993, 
+    hasGirlfriend: false,
+    lahguafes: ['ru', 'en', 'de'],
+    getFullName: function () {
+        console.log(person.firstName + ' ' + person.lastName)
+    }
+
+ };
+
+console.log(person.year);
+console.log(person['firstName']);
+
+2:17
