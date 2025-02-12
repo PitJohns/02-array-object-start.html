20e9d429e8af96bc93a88782fdfe590fea3033a5
Veniamin Evstratov <dzimmutenno@gmail.com>
2025-02-12 19:56:21 +0500

    3:16

--- app.js
+++ app.js
@@ -36,9 +36,26 @@ render();*/
     const newNote = {
       title: inputElement.value,
       completed: false,   };
-    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(newNote));
+    //listElement.insertAdjacentHTML('beforeend', getNoteTemplate(newNote));
+    notes.push(newNote);  
+    render();
     inputElement.value = '';  
  };
+
+listElement.onclick = function (event) {
+   // console.log(event.target.dataset.index) 
+   if (event.target.dataset.index) {
+    const index = parseInt(event.target.dataset.index)
+    const type = event.target.dataset.type
+
+    if (type === 'toggle') {
+        notes[index].completed = !notes[index].completed
+    } else if (type === 'remove') {
+        notes.splice(index,  1) 
+    }
+    render()
+   }
+}
 
  function getNoteTemplate(note, index) {
     return `
@@ -49,8 +66,8 @@ render();*/
         ${note.title}</span>
         <span>
             <span class="btn btn-${note.completed ? 'warning' : 'success'
-            }" data-index="${index}">&check;</span>
-            <span class="btn btn-small btn-danger">&times;</span>
+            }" data-index="${index}" data-type="toggle">&check;</span>
+            <span class="btn btn-small btn-danger" " data-index="${index}" data-type="remove">&times;</span>
         </span>
         </li>`
  };
@@ -93,10 +110,13 @@ const notes = [
 ];
 
 function render() {
+  listElement.innerHTML = ''; 
+  if (notes.length === 0) {
+    listElement.innerHTML = '<p>Нет элементов</p>';
+  }
   for (let i = 0; i < notes.length; i++) {
   listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
  }
 };
 render();
 
-2:32
---
+++ array-theory.js
@@ -0,0 +1,56 @@
+const names =['Владилен', 'Елена', 'Игорь', 'Ксения'];
+
+//names.push('Алена');
+//names.unshift ('Алена');
+//names.shift;
+
+//const firstName = names.shift();
+/*const firstName = names.pop();
+console.log(firstName);
+console.log('Names: ', names);*/
+
+//console.log(names.reverse());
+const letters = ['e', 'c', 'd', 'b', 'a'];
+//const reversed = names.toReversed();
+/*console.log(
+    letters.toSorted(function (a, b) {
+      return a.charCodeAt(0 ) - b.charCodeAt(0)
+    })
+);
+console.log(letters );*/
+//console.log(names.toSpliced(0, 2 ));
+//console.log(names);
+
+//const greateWoman = 'Елена';
+//const index = names.indexOf(greateWoman);
+//console.log(index);
+//const newNames = names.with(index, 'Елена великая');
+
+/*names[index] = 'Елена Вликая';
+console.log(names[index]);
+console.log(names)*/
+//console.log(names);
+//console.log(newNames);
+
+
+// Time 3:06
+/*const capitalNames = names.map(function(name, index) {
+    if (index === 1 ) {
+      return 'Елена Великая'
+    } 
+    return name;
+}); */
+
+//console.log(names.includes('Игорь'));
+//console.log(names.indexOf('Игорь') !== -1 );
+
+const people = [
+  {name: 'Владилен', budget: 4200},
+  {name: 'Елена', budget: 15100},
+  {name: 'Игорь', budget: 300},
+  {name: 'Ксения', budget: 7520},
+]; 
+
+ //console.log(people.indexOf({budget: 7520 }));
+
+ 3:16
