9f8d1f44944e561cec209da28bc30ba1d2ff4bd7
Veniamin Evstratov <dzimmutenno@gmail.com>
2025-02-24 12:28:53 +0500

    end block 2

--- array-theory.js
+++ array-theory.js
@@ -60,13 +60,47 @@ const people = [
     findedPerson = person
   };
  };*/
+ /*const finded = people.find(function (person) {
+  return person.budget === 7520 
+})*/
 
-const findedPerson = people.find(function
-  (person) {
-    if (person.budget === 7520) {
-      return true
-    }
+
+/*const finded = people.findIndex(function (person) {
+    return person.budget === 7520 
 })
 
- console.log(findedPerson);
- 3Ж:18:59
+//const finded = people.find((p) => p.budget === 7520)
+
+ console.log(people.with(finded, 42));*/
+/*let sumBudget = 0
+const filtered = people.filter(function(p) {
+   return p.budget > 5000
+ })
+
+console.log(filtered)
+filtered.forEach(function(p) {
+   sumBudget += p.budget
+}) 
+console.log(sumBudget)*/
+
+/*const byBudget = p => p.budget > 5000
+const pickBudget = (p) => p.budget 
+
+const sumBudget = people
+  .filter(byBudget)
+  .map(pickBudget)
+  .reduce((acc, p) => acc + p, 0)
+
+
+console.log(sumBudget)*/
+
+const string = 'Првет, как дела?'
+const reversed = string
+.split('')
+.toReversed()
+.join('!')
+.split('')
+.filter(c => c != '!')
+.join('')
+
+console.log(reversed)
